6e41c646e0464d76952f5013df42ec7af28cfc5b
Marc <you@example.com>
2014-12-14 17:11:14 -0500

    updates

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
 var WebSocketServer = require("ws").Server;
 var server = new WebSocketServer({port: 3000});
-server.on("connection", function(ws) //{
-//   console.log("Client connected!");
-//   connected +++;
-//   console.log("connected users are" + connected);
-//   ws.send("hello")
-//   ws.on ("message", function(message){
-//   if (message === "!") {
-//     console.log(message);
-//   };
-//   });
-// });
+server.on("connection", function(ws) {
+  console.log("Client connected!");
+});
